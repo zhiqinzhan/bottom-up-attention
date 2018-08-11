bd569564969ad26123a85d51ff028d189ce23c82
ZhiQin Zhan <zqzhan14@fudan.edu.cn>
2018-08-11 20:44:53 +0800

    lower the GPU memory need

--- tools/generate_tsv.sh
+++ tools/generate_tsv.sh
@@ -1,7 +1,7 @@
-python /tools/generate_tsv.py \
+python ./tools/generate_tsv.py \
     --gpu 0,1,2,3 \
     --cfg experiments/cfgs/faster_rcnn_end2end_resnet.yml \
     --def models/vg/ResNet-101/faster_rcnn_end2end_final/test.prototxt \
-    --out train2014_resnet101_faster_rcnn_genome.tsv \
+    --out output/val2014_resnet101_faster_rcnn_genome.tsv \
     --net data/faster_rcnn_models/resnet101_faster_rcnn_final.caffemodel \
-    --split coco_train2014
+    --split coco_val2014
